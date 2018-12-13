34e73fe5d875b19fbffecbddf69a719a9fe1b420
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2018-12-13 15:36:51 +0300

    Выполнено третье задание

---
+++ Homework/Distance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = ReadDouble("x1");
+            var y1 = ReadDouble("y1");
+            var x2 = ReadDouble("x2");
+            var y2 = ReadDouble("y2");
+            Console.WriteLine("Расстояние между точками: {0:F2}", Distance(x1,y1,x2,y2));
+            Console.ReadLine();
+        }
+
+        static double ReadDouble(string name)
+        {
+            string tmp = null;
+            Console.WriteLine($"{name}:");
+            while (tmp is null || tmp == "")
+                tmp = Console.ReadLine();
+            return double.Parse(tmp);
+        }
+
+        static double Distance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        }
+    }
+}
