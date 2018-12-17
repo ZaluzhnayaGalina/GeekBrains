b82e27e9b2b83aa71c2b45101c133aed3db2e7a7
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2018-12-17 15:19:36 +0300

    Выполнено 4 задание

---
+++ Homework/Methods/Auth/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int attemts = 0;
+            bool check = false;
+            do
+            {
+                var login = Console.ReadLine();
+                var password = Console.ReadLine();
+                check = Check(login, password);
+                attemts++;
+            } while (attemts<3 && !check);
+            if (check)
+                Console.WriteLine("Авторизация пройдена успешно");
+            else
+                Console.WriteLine("Авторизация не пройдена");
+            Console.ReadLine();
+        }
+
+        static bool Check(string login, string pasword)
+        {
+            return login == "root" && pasword == "GeekBrains";
+        }
+    }
+}
