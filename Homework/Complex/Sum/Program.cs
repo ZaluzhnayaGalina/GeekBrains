305c9b252e449287b62587a9e190c653b129dbd1
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2018-12-21 01:35:19 +0300

    Task2

---
+++ Homework/Complex/Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int sum = 0;
+            do
+            {
+                Console.WriteLine("Введиете целое число:");
+                if (int.TryParse(Console.ReadLine(), out x))
+                {
+                    if (x % 2 > 0)
+                    {
+
+                        sum += x;
+                    }
+                }
+                else
+                {
+                    x = 1;
+                    Console.WriteLine("Введены неверные данные!");
+                }
+
+            }
+            while (x != 0);
+            Console.WriteLine($"Сумма: {sum} ");
+            Console.ReadLine();
+        }
+    }
+}
