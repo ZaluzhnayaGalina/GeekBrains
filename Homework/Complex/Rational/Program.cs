8bd77fc3e3cd80acde76bf2a0d9f7d0e169e796d
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2018-12-21 14:46:09 +0300

    Добавлен класс рациональных чисел

---
+++ Homework/Complex/Rational/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational
+{
+    class Program
+    {
+        /*Залужная
+         * 3. *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел.
+         * Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать программу, демонстрирующую все разработанные элементы класса.
+    ** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение
+    ArgumentException("Знаменатель не может быть равен 0");
+    Добавить упрощение дробей.*/
+        static void Main(string[] args)
+        {
+           var r1 = new Rational(3,15);
+            Console.WriteLine($"r1: {r1}");
+            r1.Simplify();
+            Console.WriteLine($"Упростили: {r1}");
+            try
+            {
+                var r2 = new Rational(3,0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+            var r3 = new Rational(2,-5);
+            Console.WriteLine($"r3: {r3}");
+            Console.WriteLine($"r1 + r3 = {r1+r3}");
+            Console.WriteLine($"r1 - r3 = {r1 - r3}");
+            Console.WriteLine($"r1 * r3 = {r1 * r3}");
+            Console.WriteLine($"r1 / r3 = {r1 / r3}");
+            int r4 = 3;
+            Console.WriteLine($"r4: {r4}");
+            Console.WriteLine($"r1 + r4 = {r1 + r4}");
+            Console.WriteLine($"r1 - r4 = {r1 - r4}");
+            Console.WriteLine($"r1 * r4 = {r1 * r4}");
+            Console.WriteLine($"r1 / r4 = {r1 / r4}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework/Complex/Rational/Rational.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational
+{
+    public class Rational
+    {
+        private int _num;
+        private int _denom;
+
+        public int Num
+        {
+            get => _num;
+            set { _num = value; }
+        }
+
+        public int Denom
+        {
+            get => _denom;
+            set
+            {
+                if (value==0)
+                    throw new ArgumentException("Знаменатель не может быть равен 0!");
+                _denom = value;
+            }
+        }
+
+        public Rational(int a, int b)
+        {
+
+            if (b > 0)
+            {
+                Num = a;
+                Denom = b;
+            }
+            else
+            {
+                Num = -a;
+                Denom = -b;
+            }
+        }
+
+        public void Simplify()
+        {
+            int max = Math.Max(Math.Abs(_num), _denom);
+            int min = Math.Min(Math.Abs(_num), _denom);
+            int r;
+            do
+            {
+                r = max % min;
+                max = min;
+                min = r;
+
+            } while (r!=0);
+            _num /= max;
+            _denom /= max;
+
+        }
+
+        public override string ToString()
+        {
+            if (Denom == 1)
+                return Num.ToString();
+            return Num + "/" + Denom;
+        }
+
+        public static Rational operator +(Rational r1, Rational r2)
+        {
+            var r = new Rational(r1.Num*r2.Denom+r1.Denom*r2.Num, r1.Denom*r2.Denom);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator -(Rational r1, Rational r2)
+        {
+            var r = new Rational(r1.Num * r2.Denom - r1.Denom * r2.Num, r1.Denom * r2.Denom);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator *(Rational r1, Rational r2)
+        {
+            var r = new Rational(r1.Num * r2.Num, r1.Denom * r2.Denom);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator /(Rational r1, Rational r2)
+        {
+            var r = new Rational(r1.Num * r2.Denom , r1.Denom * r2.Num);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator +(Rational r1, int r2)
+        {
+            var r = new Rational(r1.Num + r1.Denom * r2, r1.Denom);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator -(Rational r1, int r2)
+        {
+            var r = new Rational(r1.Num - r1.Denom * r2, r1.Denom);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator *(Rational r1, int r2)
+        {
+            var r = new Rational(r1.Num * r2, r1.Denom);
+            r.Simplify();
+            return r;
+        }
+        public static Rational operator /(Rational r1, int r2)
+        {
+            var r = new Rational(r1.Num, r1.Denom * r2);
+            r.Simplify();
+            return r;
+        }
+
+    }
+}
