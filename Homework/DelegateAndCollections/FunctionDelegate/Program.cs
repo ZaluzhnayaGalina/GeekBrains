662678e1dbf9af74faa2a901abab5a063bda1966
Zaluzhnaya Galina <zaluzhnaya_galya@mail.ru>
2019-01-09 10:47:14 +0300

    Добавлен текст задания

--- Homework/DelegateAndCollections/FunctionDelegate/Program.cs
+++ Homework/DelegateAndCollections/FunctionDelegate/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+    /*Залужная
+     * 1. Изменить программу вывода функции так, чтобы можно было передавать функции типа double (double, double). 
+     * Продемонстрировать работу на функции с функцией a* x^2 и функцией a* sin(x).*/
 namespace FunctionDelegate
 {
     delegate double FuncWithParam(double param, double x);
