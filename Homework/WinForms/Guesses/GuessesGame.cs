f75094fcec5047b0587b4b80bd7a8b3f7d0a7d3e
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-01-14 23:23:12 +0300

    Add class for game logic

---
+++ Homework/WinForms/Guesses/GuessesGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Guesses
+{
+    public class GuessesGame
+    {
+        private int _answer;
+        private int _higherBound;
+        private int _maxAttemptsCount;
+        private int _attemptsCount;
+        public GuessesGame()
+        {            
+            _higherBound = 100;
+            _answer = (new Random()).Next(100);
+            _maxAttemptsCount = (int)Math.Round(Math.Log(_higherBound, 2));
+        }
+        public bool UserAnswerIsGreater(int answer)
+        {
+            return answer > _answer;
+        }
+        public bool UserAnswerIsLess(int answer)
+        {
+            return answer < _answer;
+        }
+        public bool Attempt(int answer)
+        {
+            _attemptsCount++;
+            return answer == _answer;            
+        }
+        public bool CheckAttemptPosibility()
+        {
+            _attemptsCount++;
+            return _attemptsCount <= _maxAttemptsCount;
+        }
+    }
+}
