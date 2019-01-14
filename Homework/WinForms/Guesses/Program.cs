5cbc81a91614495832803d10b4cd1a69a8cc87af
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-01-15 01:43:26 +0300

    Add tasks' descriptions

--- Homework/WinForms/Guesses/Program.cs
+++ Homework/WinForms/Guesses/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Guesses
 {
+    /*Залужная
+     * 2. Используя Windows Forms, разработать игру «Угадай число».
+     * Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток.
+     * Для ввода данных от человека используется элемент TextBox.*/
     static class Program
     {
         /// <summary>
