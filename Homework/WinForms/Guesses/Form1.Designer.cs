3f907ce9b9f75c72664838e1c2f86fdc0a5b1622
ZaluzhnayaGalina <zaluzhnaya_galya@mail.ru>
2019-01-14 23:05:04 +0300

    Created a WinForm for Guesses

---
+++ Homework/WinForms/Guesses/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Guesses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.atteptsList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(12, 103);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(100, 23);
+            this.checkButton.TabIndex = 0;
+            this.checkButton.Text = "Я прав?";
+            this.checkButton.UseVisualStyleBackColor = true;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 47);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // atteptsList
+            // 
+            this.atteptsList.FormattingEnabled = true;
+            this.atteptsList.Location = new System.Drawing.Point(152, 47);
+            this.atteptsList.Name = "atteptsList";
+            this.atteptsList.Size = new System.Drawing.Size(120, 95);
+            this.atteptsList.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите число:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Предыдущие попытки:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.atteptsList);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.checkButton);
+            this.Name = "Form1";
+            this.Text = "Угадайка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.ListBox atteptsList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
